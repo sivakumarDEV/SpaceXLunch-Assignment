48e7435a54ef67d1751bfc4a270e021b3fbf1feb
Sivakumar K <kalisettysivakumar14@gmail.com>
2020-08-18 09:47:51 +0530

    deploying to heroku

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/space-x-app'));
+app.get('/*', function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/space-x-app/index.html'));});
+    app.listen(process.env.PORT||8080);
