230b0ce275222e80bbd409df990106111914b632
Sivakumar K <kalisettysivakumar14@gmail.com>
2020-08-18 10:58:13 +0530

    deploy space on heroku

--- server.js
+++ server.js
@@ -5,3 +5,6 @@ app.use(express.static(__dirname + '/dist/space-x-app'));
 app.get('/*', function(req,res){
     res.sendFile(path.join(__dirname+'/dist/space-x-app/index.html'));});
     app.listen(process.env.PORT||8080);
+
+
+    
